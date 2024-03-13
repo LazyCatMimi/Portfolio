52e1447be012c809f07033bf55bc1ebae53cb372
Quynh Vo <quynh.vo3314@gmail.com>
2024-03-13 13:56:55 -0400

    more basic stuff

---
+++ src/Pages/Home.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      <p>meow</p>
+      <h1>Hello, I am</h1>
+      <h1>Quynh Vo</h1>
+      <h1>Artist. Designer. Developer</h1>
+      <button>See Resume</button>
+    </div>
+  );
+}
