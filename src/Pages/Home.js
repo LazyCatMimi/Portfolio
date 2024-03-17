24264ea935504e60a00bad599d67cafc6101ab0e
Quynh Vo <quynh.vo3314@gmail.com>
2024-03-17 16:18:53 -0400

    some more styles

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,7 +5,7 @@ import "../Styles/Home.css";
 import uiuxIcon from "../Assets/focus-uiux.svg";
 import devIcon from "../Assets/focus-dev.svg";
 import artIcon from "../Assets/focus-art.svg";
-import plImg from "../Assets/test.png"
+import plImg from "../Assets/test.png";
 
 export default function Home() {
   return (
@@ -15,7 +15,11 @@ export default function Home() {
         <div>
           <h1 class="title-sub-1">Hello, I am</h1>
           <h1 class="title-name">Quynh Vo</h1>
-          <h1 class="title-sub-2">Artist. Designer. Developer</h1>
+          <h1 class="title-sub-2">
+            Artist<span style={{ color: "white" }}>.</span> Designer
+            <span style={{ color: "white" }}>.</span> Developer
+            <span style={{ color: "white" }}>.</span>
+          </h1>
           <button className="primary-button">See Resume</button>
         </div>
       </header>
@@ -35,7 +39,7 @@ export default function Home() {
         </div>
       </section>
       <section className="light-bg" id="about">
-       <img src={plImg}></img>
+        <img src={plImg}></img>
         <article>
           <h2>About Me</h2>
           <p>
@@ -51,46 +55,44 @@ export default function Home() {
           </p>
         </article>
       </section>
-      <section className="dark-bg">
-  
-          <h1>Latest Projects</h1>
-          <p>01</p>
-          <article className="dark-bg2">
-            <p>picture here</p>
-            <h3>The Suite Spot Salon</h3>
-            <p>Web Design & Development</p>
-            <p>Tools Used: Figma</p>
-            <p>Technologies: React.JS, Node.JS, Express.JS, MongoDB Atlas</p>
-            <p>
-              Project Overview: <br />
-              The Suite Spot Salon is a fictitious salon suite client developed
-              by our team for our senior design project. This is a 2-semester
-              project consisting of both the design and development phases. Our
-              website allows users to conveniently book appointments, purchase
-              items from the shop, and manage their accounts.
-            </p>
-            <button className="primary-button">Read More</button>
-          </article>
-          <p>02</p>
-          <article className="dark-bg2">
-            <p>picture here</p>
-            <h3>Noodletopia</h3>
-            <p>Mobile App Design</p>
-            <p>Tools Used: Figma</p>
-            <p>
-              Project Overview: <br />
-              Noodletopia is a mobile application designed for noodle
-              enthusiasts, offering a personalized experience for ordering and
-              creating customized noodle bowls with various toppings. Users have
-              the freedom to add reviews for their noodle creations and share
-              their unique combinations with others. Additionally, they can save
-              their favorite noodle bowls for easy access. The entire user
-              design process was meticulously documented, incorporating user
-              testing to refine and enhance the app's design and usability.
-            </p>
-            <button className="primary-button">Read More</button>
-          </article>
-        
+      <section className="dark-bg" id="latest-projects">
+        <h2>Latest Projects</h2>
+        <p>01</p>
+        <article className="dark-bg2">
+          <p>picture here</p>
+          <h3>The Suite Spot Salon</h3>
+          <p>Web Design & Development</p>
+          <p>Tools Used: Figma</p>
+          <p>Technologies: React.JS, Node.JS, Express.JS, MongoDB Atlas</p>
+          <p>
+            Project Overview: <br />
+            The Suite Spot Salon is a fictitious salon suite client developed by
+            our team for our senior design project. This is a 2-semester project
+            consisting of both the design and development phases. Our website
+            allows users to conveniently book appointments, purchase items from
+            the shop, and manage their accounts.
+          </p>
+          <button className="primary-button">Read More</button>
+        </article>
+        <p>02</p>
+        <article className="dark-bg2">
+          <p>picture here</p>
+          <h3>Noodletopia</h3>
+          <p>Mobile App Design</p>
+          <p>Tools Used: Figma</p>
+          <p>
+            Project Overview: <br />
+            Noodletopia is a mobile application designed for noodle enthusiasts,
+            offering a personalized experience for ordering and creating
+            customized noodle bowls with various toppings. Users have the
+            freedom to add reviews for their noodle creations and share their
+            unique combinations with others. Additionally, they can save their
+            favorite noodle bowls for easy access. The entire user design
+            process was meticulously documented, incorporating user testing to
+            refine and enhance the app's design and usability.
+          </p>
+          <button className="primary-button">Read More</button>
+        </article>
       </section>
       <section>
         <h2>Explore More Projects</h2>
