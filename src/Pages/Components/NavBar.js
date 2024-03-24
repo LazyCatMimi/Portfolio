427d32daacf12611b3b542988730f7a8a324c37b
Quynh Vo <quynh.vo3314@gmail.com>
2024-03-23 22:59:22 -0400

    responsive navbar

--- src/Pages/Components/NavBar.js
+++ src/Pages/Components/NavBar.js
@@ -1,37 +1,61 @@
-import "../../Styles/Components/NavBar.css";
-import logoIcon from "../../Assets/logo.png";
-import menuIcon from "../../Assets/Icons/menu.svg";
-import { useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import PATHS from "../../Data/Pages.json";
+import logoIcon from "../../Assets/logo.png";
+import menuIcon from "../../Assets/Icons/menu.svg";
+import xIcon from "../../Assets/Icons/x.svg";
+import "../../Styles/Components/NavBar.css";
+
 export default function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="logo">
         <img src={logoIcon} alt="logo" />
       </div>
       <div className="menu">
-        {/* <img src={menuIcon} alt="menu" /> */}
-        <ul>
+        <img
+          src={menuOpen ? xIcon : menuIcon}
+          alt="menu"
+          className="menu-icon"
+          onClick={toggleMenu}
+        />
+        <ul className={`menu-items ${menuOpen ? "active" : ""}`}>
           <li>
-            <Link to={PATHS.main.home}>Home</Link>
+            <Link to={PATHS.main.home} onClick={toggleMenu}>
+              Home
+            </Link>
           </li>
           <li>
-            <Link to={PATHS.main.webDesign}>Web Design</Link>
+            <Link to={PATHS.main.webDesign} onClick={toggleMenu}>
+              Web Design
+            </Link>
           </li>
           <li>
-            <Link to={PATHS.main.webDev}>Web Dev</Link>
+            <Link to={PATHS.main.webDev} onClick={toggleMenu}>
+              Web Dev
+            </Link>
           </li>
           <li>
-            <Link to={PATHS.main.artDesign}>Art & Design</Link>
+            <Link to={PATHS.main.artDesign} onClick={toggleMenu}>
+              Art & Design
+            </Link>
           </li>
           <li>
-            <Link to={PATHS.main.resume}>Resume</Link>
+            <Link to={PATHS.main.resume} onClick={toggleMenu}>
+              Resume
+            </Link>
           </li>
           <li>
-            <Link to={PATHS.main.contact}>Contact</Link>
-          </li>{" "}
-          {/* Link to the contact page */}
+            <Link to={PATHS.main.contact} onClick={toggleMenu}>
+              Contact
+            </Link>
+          </li>
         </ul>
       </div>
     </nav>
