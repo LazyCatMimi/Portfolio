cec78b3c8f922e77218b145d35f08bc5c57d7827
Quynh Vo <quynh.vo3314@gmail.com>
2024-09-03 21:57:52 -0400

    title to iframe

--- src/Pages/Projects/Thesuitespot.js
+++ src/Pages/Projects/Thesuitespot.js
@@ -352,7 +352,7 @@ export default function Thesuitespot() {
             alt=""
           ></img>
           <iframe
-            title="buh"
+            title="Design guide"
             style={{ border: "1px solid rgba(0, 0, 0, 0.1)" }}
             width="800"
             height="450"
