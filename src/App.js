25aa6c1bf2c1ca1bda721ca74ceb21a99f9bc0dd
Quynh Vo <quynh.vo3314@gmail.com>
2025-03-23 23:02:57 -0400

    moreeeee

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import NavBar from "./Pages/Components/NavBar";
 import Footer from "./Pages/Components/Footer";
-import ProjectsWebDesign from "./Pages/ProjectsWebDesign";
+// import ProjectsWebDesign from "./Pages/ProjectsWebDesign - OLD";
 import Resume from "./Pages/Resume";
 import Contact from "./Pages/Contact - OLD";
 import PATHS from "./Data/Pages.json";
 import ScrollToTop from "./Pages/Components/ScrollToTop";
 import Project from "./Pages/Project";
-import { inject } from '@vercel/analytics';
-import { injectSpeedInsights } from '@vercel/speed-insights';
+import { inject } from "@vercel/analytics";
+import { injectSpeedInsights } from "@vercel/speed-insights";
 
 function App() {
   return (
@@ -20,7 +20,7 @@ function App() {
         <Routes>
           <Route path={PATHS.main.home} element={<Home />} />
           {/* <Route path={PATHS.main.contact} element={<Contact />} /> */}
-          <Route path={PATHS.main.webDesign} element={<ProjectsWebDesign />} />
+          {/* <Route path={PATHS.main.webDesign} element={<ProjectsWebDesign />} /> */}
           <Route path={PATHS.main.resume} element={<Resume />} />
           <Route path="/projects/:id" element={<Project />} />
 
