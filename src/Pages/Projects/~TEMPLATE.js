47fe031ee4dc25ffdbcb8774d1ccc56636d703f2
Quynh Vo <quynh.vo3314@gmail.com>
2025-07-29 21:09:03 -0400

    75ch

--- src/Pages/Projects/~TEMPLATE.js
+++ src/Pages/Projects/~TEMPLATE.js
@@ -1,6 +1,6 @@
 export default function TEMPLATE() {
   return (
-    <div className="proj-more-info" id="ucfredo">
+    <div className="proj-more-info">
       <article className="proj-info pi-after">
         <p>TEMPLATE</p>
       </article>
@@ -31,10 +31,10 @@ export const ExternalLinkButton = ({ href, text }) => (
 
 export const Section = ({ title, children, line=true }) => (
   <article className="proj-info pi-after">
+    {line ? <hr class="line"></hr> : null}
     <h3 className="center-text" id={generateIdFromText(title)}>
       {title}
     </h3>
-    {line ? <hr class="line"></hr> : null}
     {children}
   </article>
 );
